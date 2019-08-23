19312fc62815fc1538f881f50f3f1250b5e2e1e9
Parker Storck <pstorck6@gmail.com>
2019-08-23 13:27:04 -0500

    Fan turns on when temp is too high

--- test.py
+++ test.py
@@ -1,14 +1,21 @@
 import Adafruit_DHT as DHT
 import time, sqlite3
 from datetime import datetime
+import RPi.GPIO as GPIO
 
 conn = sqlite3.connect('gardenData.db')
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
 
-for i in range(10):
+while True:
 	c = conn.cursor()
 	humidity, temperature = DHT.read_retry(DHT.DHT22, 3)
+	if temperature > 25:
+		GPIO.output(17, GPIO.HIGH)
+	else:
+		GPIO.output(17, GPIO.LOW)
 	print('Humidity: {} Temperature: {}'.format(humidity, temperature))
 	c.execute("INSERT INTO dhtreadings (timestamp,temperature,humidity) VALUES(?,?,?)",  (datetime.now(), humidity, temperature))
 	conn.commit()
 	c.close()
-	time.sleep(5)
+	time.sleep(1)
